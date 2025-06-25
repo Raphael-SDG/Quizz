9a51c318420393412f6c7e53993f497e8a067049
RaphaelSpencerDaGraca <oversquad04@gmail.com>
2025-06-26 01:26:31 +0200

    Premier commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("buttonOUI").addEventListener("click", function () {
+    window.location.href = "quizz.html";
+});
+
+document.getElementById("buttonNON").addEventListener("click", function () {
+    alert("Dommage... 👮‍♂️");
+});
